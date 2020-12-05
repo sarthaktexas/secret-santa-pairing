1e0adad792b016a673440cf43f7afcf64e90d38a
Sarthak Mohanty <sarthaktexas@gmail.com>
2020-12-05 06:20:28 +0000

    🚀 now pairs circularly and by region

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 require('colors');
+const axios = require('axios');
 const AirtablePlus = require('airtable-plus');
 const usersTable = new AirtablePlus({
   baseID: 'appErDYUv6Oppe9Cc',
@@ -12,29 +13,40 @@ const matchingTable = new AirtablePlus({
   apiKey: process.env.AIRTABLE_API_KEY,
   tableName: 'Matching',
 });
-const users = [];
 
 /**
  * Main Function, yes i know bad code whatever
  */
 const main = async () => {
-  await getUsers();
-  PairUsers(users);
+  // gets gifters from each area
+  await getUsers('North America').then(users => PairUsers(users));
+  await getUsers('South America').then(users => PairUsers(users));
+  await getUsers('Europe').then(users => PairUsers(users));
+  await getUsers('India').then(users => PairUsers(users));
+  await getUsers('Asia except India').then(users => PairUsers(users));
+  await getUsers('Africa').then(users => PairUsers(users));
 }
 
-main(); // run the app
+main().then(() => console.log(`I've finished matching everybody. Happy Gifting!`.bgCyan.black)); // run the app
 
 /**
  * Gets Gifters from Airtable
  */
-async function getUsers() {
-  const userRes = await usersTable.read();
+async function getUsers(region) {
+  const userRes = await usersTable.read({
+    filterByFormula: `region = "${region}"`
+  });
+  const users = [];
   userRes.forEach(user => {
     users.push({
       email: user.fields.email,
       id: user.fields.id,
+      region: user.fields.region,
+      address: user.fields.address,
+      likes: user.fields.likes,
     });
   });
+  return (users);
 }
 
 /**
@@ -42,27 +54,32 @@ async function getUsers() {
  * @param {Array} users Array of users following format in README
  */
 function PairUsers(users) {
-  if (!isEven(users.length)) throw 'The array given has an odd number of gifters.'.bgRed.white;
-    shuffle(users);
-    const half = Math.ceil(users.length / 2);
-    const firstHalf = users.splice(0, half);
-    const secondHalf = users.splice(-half);
-    let i = 0;
-  firstHalf.forEach(user => {
-    console.log(`I'm pairing ${(user.id).red} with ${(secondHalf[i].id).green}`)
-    i++
-  })
-}
-
-/**
- * Check if number is even or odd
- * @param {Number} value number to check
- */
-function isEven(value) {
-	if (value%2 == 0)
-		return true;
-	else
-		return false;
+  shuffle(users);
+  users.forEach(async (user, i) => {
+    let user_1 = user;
+    let user_2;
+    if (i !== users.length - 1) {
+      user_2 = users[i + 1];
+      console.log(`I'm pairing ${(user_1.id).red} (${user_1.region.gray}) with ${(user_2.id).green} (${user_2.region.gray})`);
+    } else {
+      user_2 = users[0];
+      console.log(`I'm pairing ${(user_1.id).red} (${user_1.region.gray}) with ${(user_2.id).green} (${user_2.region.gray})`)
+    }
+    const matchRecord = await matchingTable.create({ id: user_1.id, match: user_2.id, regionmatch: user_1.region === user_2.region ? true : false, region: user_1.region }).catch(err => console.log(err));
+    const webhookLink = "https://hooks.slack.com/workflows/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"
+    axios({
+      method: 'post',
+      url: webhookLink,
+      data: {
+        userId: user_1.id,
+        matchedUser: user_2.id,
+        address: user_2.address,
+        recordId: matchRecord.id,
+        likes: user_2.likes,
+        userIdLiteral: user_1.id,
+      },
+    });
+  });
 }
 
 /**
